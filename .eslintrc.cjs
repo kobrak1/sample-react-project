24e19784a492b03203c4cd0885396238c083bf8b
kobrak1 <burakkarhan657@gmail.com>
2023-10-15 00:09:12 +0300

    sample react project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,4 +17,5 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  "ract/prop-types": 0,
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 
+// display component
+const Display = ({counter}) => <div> {counter} </div>
 
-function App() {
+// button component
+const Button = ({eventFunc, btnName}) => <button onClick={eventFunc}> {btnName} </button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // functions declared
+  const plus = () => setCounter(counter + 1);
+  const minus = () => setCounter(counter - 1);
+  const reset = () => setCounter(0)
 
   return (
     <>
-      Test
+      <Display counter={counter} />
+      <Button eventFunc={plus} btnName={"+"} />
+      <Button eventFunc={minus} btnName={"-"} />
+      <Button eventFunc={reset} btnName={"Reset"} />
     </>
   )
 }
 
-export default App
+export default App;
